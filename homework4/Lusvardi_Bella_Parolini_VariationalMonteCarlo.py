2c578fe65a7936cd20e0d58627ebf98bbc32b2d6
Marco Bella <bellamarco01@gmail.com>
2025-06-23 15:56:30 +0200

    functional jupytext setting .?

--- homework4/Lusvardi_Bella_Parolini_VariationalMonteCarlo.py
+++ homework4/Lusvardi_Bella_Parolini_VariationalMonteCarlo.py
@@ -1,15 +1,37 @@
-# %%
-import random
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.17.2
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# +
 import numpy as np
+import scipy
+import sys
 import math
+import scipy.special
+import scipy.constants as const
+from math import factorial
+from scipy.special import hermite
+import matplotlib.pyplot as plt
 import random
 
+print('tiocfaidh ar la v5')
+# -
 
-# %% [markdown]
 # In order to find the values of $\alpha$ that satisfy the cusp condition we notice that
-# $$\lim_{r_{ij}\rightarrow 0}\frac{H\Psi}{\Psi}<\infty$$
+# $$lim_{r_{ij}\rightarrow 0}\frac{H\Psi}{\Psi}<\infty$$
 # where $H=H_0+V$ ,is equivalent to 
-# $$\lim_{r_{ij}\rightarrow 0}\frac{\Psi'}{\Psi}=\frac{1}{2}r_{ij}V$$
+# $$lim_{r_{ij}\rightarrow 0}\frac{\Psi'}{\Psi}=\frac{1}{2}r_{ij}V$$
 # having
 # $$V=\frac{1}{2}\omega^2r_i^2+\frac{1}{r_{ij}}$$
 # Substituting in this condition the Jastrow function for antiparallel spins and Jastrow function multiplied by $r$ fo parallel spins we find 
@@ -19,7 +41,7 @@ import random
 #
 #
 #
-# Since we are interested in the cases up to $N=6$ let us consider only the lower energy states in analitycal form, using the notation $\phi_{nlm}$
+# Since we are interested in the cases up to $N=6$ let's consider only the lower eergy states in analitycal form, using the notation $\phi_{nlm}$
 # $$\phi_{000}=\frac{1}{\sqrt{\pi\sigma^2}}e^{\frac{r^2}{2\sigma^2}}$$ 
 # $$\phi_{01-1}=\frac{1}{\sqrt{\pi\sigma^2}}e^{\frac{r^2}{2\sigma^2}}\left(\frac{r}{\sigma}\right)e^{-i\varphi}$$ 
 # $$\phi_{011}=\frac{1}{\sqrt{\pi\sigma^2}}e^{\frac{r^2}{2\sigma^2}}\left(\frac{r}{\sigma}\right)e^{i\varphi}$$ 
@@ -35,12 +57,10 @@ import random
 # $$\chi_+=\frac{\phi_{011}+\phi_{01-1}}{2}=\frac{1}{\sqrt{\pi\sigma^2}}e^{\frac{x^2+y^2}{2\sigma^2}}x$$
 # $$\chi_-=\frac{\phi_{011}-\phi_{01-1}}{2i}=\frac{1}{\sqrt{\pi\sigma^2}}e^{\frac{x^2+y^2}{2\sigma^2}}y$$
 
-# %%
 N=6
 omega=1
 
 
-# %%
 def Slaterdet(N,s,x1,y1,x2,y2,x3,y3,phi,chip,chim):
     A = [[(1/math.factorial(N))*phi(x1,y1,s), (1/math.factorial(N))*chip(x1,y1,s), (1/math.factorial(N))*chim(x1,y1,s)], 
          [(1/math.factorial(N))*phi(x2,y2,s), (1/math.factorial(N))*chip(x2,y2,s), (1/math.factorial(N))*chim(x2,y2,s)],
@@ -48,7 +68,6 @@ def Slaterdet(N,s,x1,y1,x2,y2,x3,y3,phi,chip,chim):
     return np.linalg.det(A)
 
 
-# %%
 def Slaterinv(N,s,x1,y1,x2,y2,x3,y3,phi,chip,chim):
     A = [[(1/math.factorial(N))*phi(x1,y1,s), (1/math.factorial(N))*chip(x1,y1,s), (1/math.factorial(N))*chim(x1,y1,s)], 
          [(1/math.factorial(N))*phi(x2,y2,s), (1/math.factorial(N))*chip(x2,y2,s), (1/math.factorial(N))*chim(x2,y2,s)],
@@ -56,34 +75,47 @@ def Slaterinv(N,s,x1,y1,x2,y2,x3,y3,phi,chip,chim):
     
     return np.linalg.inv(A)
 
-import matplotlib.pyplot as plt
 
-# %%
-def Metropolisguess(r,delta):
-    rn=np.zeros(len(r))
-    for i in range(len(r)):
-        a=random.uniform(0,1)
-        rn[i]= r[i]+delta*(a-0.5)
-    return rn
-
-# %%
+# +
 #Metropolis
-def Metropolis(r,delta, psi, b,counter, acc):
-    rn = Metropolisguess(r,delta)
-    p=psi(rn,b)**2/psi(r,b)**2
+def Metropolis(x01,y01,x02,y02,x03,y03, delta, phi, chip, chim, s,counter, acc):
+    a1=random.uniform(0,1)
+    x1= x01+delta*(a1-0.5)
+    b1=random.uniform(0,1)
+    y1= y01+delta*(b1-0.5)
+    a2=random.uniform(0,1)
+    x2= x02+delta*(a2-0.5)
+    b2=random.uniform(0,1)
+    y2= y02+delta*(b2-0.5)
+    a3=random.uniform(0,1)
+    x3= x03+delta*(a3-0.5)
+    b3=random.uniform(0,1)
+    y3= y03+delta*(b3-0.5)
+    p=Slaterdet(N,s,x1,y1,x2,y2,x3,y3,phi,chip,chim)**2/Slaterdet(N,s,x01,y01,x02,y02,x03,y03,phi,chip,chim)**2
     if p>1:
-        r=rn
+        x01=x1
+        y01=y1
+        x02=x2
+        y02=y2
+        x03=x3
+        y03=y3
         acc+=1
     else:
         xi=random.uniform(0,1)
         if p>xi:
-            r=rn
+            x01=x1
+            y01=y1
+            x02=x2
+            y02=y2
+            x03=x3
+            y03=y3
             acc+=1
 
     counter+=1
-    return r,acc, counter
+    return x01,y01,x02,y02,x03,y03, acc, counter
 
-# %%
+
+
 def adjust_delta(delta, acceptance_rate):
     if acceptance_rate > 0.6:
         delta *= 1.1  # Increase delta by 10%
@@ -91,93 +123,49 @@ def adjust_delta(delta, acceptance_rate):
         delta *= 0.9  # Decrease delta by 10%
     return delta
 
-# %%
-def psix(x, b):
-    return math.exp(-x**2/(2*b**2))
 
-def psiy(y, b):
-    return math.exp(-y**2/(2*b**2))
+def phi0(x, y, s):
+    return (1/np.sqrt(np.pi*s**2))*math.exp((x**2+y**2)/(2*s**2))
 
-def psi(x,y,b):
-    return psix(x,b)*psiy(y,b)
+def chiplus(x, y, s):
+    return (1/np.sqrt(np.pi*s**2))*math.exp((x**2+y**2)/(2*s**2))*x
 
-def psi2(r,b):
-    return psi(r[0],r[1],b)*psi(r[2],r[3],b)
+def chiminus(x, y, s):
+    return (1/np.sqrt(np.pi*s**2))*math.exp((x**2+y**2)/(2*s**2))*y
 
-def psi3(r,b):
-    return (psi(r[0],r[1],b)*r[2]*psi(r[2],r[3],b)-psi(r[2],r[3],b)*r[0]*psi(r[0],r[1],b))*psi(r[4],r[5],b)
-
-def psi4(r,b):
-    return (psi(r[0],r[1],b)*r[2]*psi(r[2],r[3],b)-psi(r[2],r[3],b)*r[0]*psi(r[0],r[1],b))*(psi(r[4],r[5],b)*r[2]*psi(r[6],r[7],b)-psi(r[6],r[7],b)*r[0]*psi(r[4],r[5],b))
-
-# %%
-def delta_choice(r,delta,b,N_delta,counter,psi):
+def delta_choice(x01,y01,x02,y02,x03,y03,delta,s,N_delta,counter):
   acc_rate=1
   while acc_rate>0.6 or acc_rate<0.4:
     acc,counter=0,0
     for i in range(N_delta):
-      r, acc,counter = Metropolis(r, delta, psi, b,counter, acc)
+      x01,y01,x02,y02,x03,y03, acc,counter = Metropolis(x01,y01,x02,y02,x03,y03, delta, phi0, chiplus,chiminus, s,counter, acc)
     acc_rate=acc/counter
     delta = adjust_delta(delta, acc_rate)
 
   return delta
 
-# %%
-def functchoice(n):
-    if n==1:
-        phi = psi
-    elif n==2:
-        phi = psi2
-    elif n==3:
-        phi = psi3
-    elif n==4:
-        phi = psi4
-    return phi
-        
 
-# %%
-b=1
+# -
 
-part = 2
-funct=functchoice(part)
-r = np.zeros(2*part)
 acc, counter=0,0
-for i in range(len(r)):
-    r[i]=random.uniform(0,1)
+x01=random.uniform(0,1)
+y01=random.uniform(0,1)
+x02=random.uniform(0,1)
+y02=random.uniform(0,1)
+x03=random.uniform(0,1)
+y03=random.uniform(0,1)
 delta=1
-N=1000000
-Neq=int(N*1/50)
+passi=10
+Neq=int(passi*1/50)
 N_delta=10000
-posx1,posy1,posx2,posy2, acc_list=[],[],[],[],[]
-sum_E, Esq, s1, s2,s1sq,s2sq= 0,0,0,0,0,0
+pos, acc_list=[],[]
+s=1
 
-# %%
-delta = delta_choice(r,delta,b,N_delta,0,funct)
+# +
+delta = delta_choice(x01,y01,x02,y02,x03,y03,delta,s,N_delta,counter)
 
-for i in range(N):
-    r,acc, counter = Metropolis(r, delta, funct, b,counter, acc)
-    posx1.append(r[0])
-    posy1.append(r[1])
-    posx2.append(r[2])
-    posy2.append(r[3])
+for i in range(passi):
+    x01,y01,x02,y02,x03,y03, acc,counter = Metropolis(x01,y01,x02,y02,x03,y03, delta, phi0, chiplus,chiminus, s,counter, acc)
+    #pos.append(x0)
     acc_list.append(acc/counter)
-
-
-print(f'b={b},\u0394={round(delta,3)}')
-
-
-# %%
-plt.plot(np.array(range(N)),acc_list)
-plt.title('Acceptance rate')
-
-# %%
-y=np.arange(0,N,1)
-plt.plot(y,posx1, label='x1')
-plt.plot(y,posy1, label='y1')
-
-# %%
-y=np.arange(0,N,1)
-plt.plot(y,posx2, label='x2')
-plt.plot(y,posy2, label='y2')
-
 
